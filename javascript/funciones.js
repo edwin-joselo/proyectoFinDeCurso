8c7397a83b95ab03ba436978a7d7aa4fe0d76fbd
edwin-joselo <64771706+edwin-joselo@users.noreply.github.com>
2021-02-10 18:19:24 +0100

    v2 formulario registro

--- javascript/funciones.js
+++ javascript/funciones.js
@@ -8,9 +8,55 @@ function setUp() {
 function mostrarFormularioRegistro() {
 
     Swal.fire({
-        title: 'Error!',
-        text: 'Do you want to continue',
-        icon: 'error',
-        confirmButtonText: 'Cool'
-    });
+        title: 'Insertar un nuevo usuario',
+        html: `
+            <form action="" class="formulario-registro">
+                <input type="hidden" value="insertar" name="tipo_operacion"/>
+                <label>DNI: </label>
+                <input type="text" name="dni" />
+
+                <label>Nombre: </label>
+                <input type="text" name="nombre" />
+
+                <label>Apellidos: </label>
+                <input type="text" name="apellidos" />
+
+                <label>Fecha de nacimiento: </label>
+                <input type="date" name="fecha_nacimiento" />
+
+                <label>Teléfono: </label>
+                <input type="text" name="telefono" />
+
+                <label>Email: </label>
+                <input type="email" name="email" />
+
+                <label>Contraseña: </label>
+                <input type="text" name="contrasenia" />
+
+                <label>Repetir contraseña: </label>
+                <input type="text" name="repetir_contrasenia" />
+
+                <label>Foto: </label>
+                <input type="file" name="foto" />
+            </form>
+        `,
+        showCancelButton: true,
+        confirmButtonColor: 'green',
+        cancelButtonColor: 'darkred',
+        confirmButtonText: 'Aceptar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                'Añadido!',
+                'Your file has been deleted.',
+                'success'
+            )
+        } else {
+            Swal.fire(
+                'Cancelado!',
+                'Your file has been deleted.',
+                'error'
+            )
+        }
+    })
 }
