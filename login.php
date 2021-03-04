1a67191c5c18701ec84c0b1ec9abe0a0bca8ba94
edwin <pilines2011@gmail.com>
2021-03-04 20:45:46 +0100

    login

--- login.php
+++ login.php
@@ -5,8 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/login/monitor.css">
 </head>
 <body>
-    <h1>Pagina principal</h1>
+    <?php 
+    include_once './maquetacion/login/cabecera.php';
+    include_once './maquetacion/login/principal.php';
+    ?>
 </body>
 </html>
---
+++ maquetacion/login/principal.php
@@ -0,0 +1,30 @@
+<main>
+    <form action="<?php ?>" method="post">
+        <div>
+            <h1>Iniciar sesión</h1>
+            <hr>
+        </div>
+        <div>
+            <p for="">Email: </p>
+            <input type="text" name="email" placeholder="Introduce tu email">
+        </div>
+        <div>
+            <p for="">Contraseña: </p>
+            <input type="password" name="contrasenia" placeholder="Introduce tu contraseña">
+        </div>
+        <input type="submit" value="Iniciar Sesión">
+        <div>
+            <p><a href="./">¿Has perdido la contraseña?</a></p>
+            <p><label for="">¿No tienes una cuenta?</label><a href="">Registrate</a></p>
+        </div>
+    </form>
+
+    <!-- <form> 
+        <div class="titulo">Registro de usuarios</div>
+
+        <input type="text" name="" id="eInputUsuario">
+        <label for="eInputUsuario">Usuario</label>
+
+        <input type="button" value="Validar">
+    </form> -->
+</main>
