f0212dd242ed674dbfa9c601a20043e02d1fd49d
mrpedrotomas <rosaurojeje@gmail.com>
2021-03-05 02:21:41 +0100

    index

--- maquetacion/index/menu.php
+++ maquetacion/index/menu.php
@@ -8,4 +8,18 @@
         <li><a id="btnLogin" href="#">Iniciar Sesión</a></li>
         <li><a href="#">Contacto</a></li>
     </ul>
+    <hr>
+    <div class="presentacion">
+        <h3>¿En qué consiste esta página web?</h3>
+        <p>Es una aplicación web que nos permite interponer denuncias on-line además de obtener información realcionada con la policía.</p>
+        <h3>¿Cómo presentar una denuncia?</h3>
+        <p>Hay que seguir los siguientes pasos:</p>
+        <ul>
+            <li>Registrarse en esta página en caso de que no tengamos una cuenta</li>
+            <li>Una vez tenemos una sesión iniciada, hacer click en la opción "poner denuncia" que aparecerá en el menú</li>
+            <li>Rellenar los datos del formulario (con la posibilidad de subir fotos).</li>
+            <li>Enviar la denuncia y esperar.</li>
+            <li>Se le enviará un correo cuando su denuncia sea aceptada.</li>
+        </ul>
+    </div>
 </nav>
