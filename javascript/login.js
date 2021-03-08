f5d0ef053cf8be41e7b80df8c6e4238712640585
edwin <pilines2011@gmail.com>
2021-03-08 20:03:55 +0100

    enlace

--- javascript/login.js
+++ javascript/login.js
@@ -1,20 +1,15 @@
 window.addEventListener('DOMContentLoaded', iniciar);
 
 function iniciar(e) {
-    document.querySelector('main>svg').addEventListener('mouseover', cambiarColor);
-    document.querySelector('main>svg').addEventListener('mouseout', quitarColor);
-    document.querySelector('main>svg').addEventListener('click', volverIndex);
+    document.querySelector('main>a>svg').addEventListener('mouseover', cambiarColor);
+    document.querySelector('main>a>svg').addEventListener('mouseout', quitarColor);
 
     function cambiarColor(e) {
-        document.querySelector('main>svg>path').setAttribute('fill', 'rgb(181, 222, 255)')
+        document.querySelector('main>a>svg>path').setAttribute('fill', 'rgb(181, 222, 255)')
     }
 
     function quitarColor(e) {
-        document.querySelector('main>svg>path').setAttribute('fill', 'white')
-    }
-
-    function volverIndex(e) {
-        window.location = './index.php';
+        document.querySelector('main>a>svg>path').setAttribute('fill', 'white')
     }
 }
 
