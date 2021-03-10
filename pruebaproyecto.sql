5eb979f57d048286257970d16913997d535cac29
mrpedrotomas <rosaurojeje@gmail.com>
2021-03-10 18:03:02 +0100

    bd

--- pruebaproyecto.sql
+++ pruebaproyecto.sql
@@ -8,6 +8,8 @@ DROP USER IF EXISTS 'admin';
 
 CREATE USER 'admin' IDENTIFIED BY 'admin';
 
+GRANT ALL PRIVILEGES ON pruebaproyecto TO 'admin';
+
 -- crear las tablas
 CREATE TABLE policias(
     num_placa varchar(7) PRIMARY KEY,
