aa28df1f9df7a291d860d1b4444816c1a673d661
edwin <pilines2011@gmail.com>
2021-03-10 20:31:21 +0100

    comprobacion empty registro

---
+++ php/funciones.php
@@ -0,0 +1,106 @@
+<?php 
+    
+    function mostrar_errores($errores) {
+        echo '<ul>';
+        foreach ($errores as $error) {
+            echo '<li>'.$error.'</li>';
+        }
+        echo '</ul>';
+    }
+    
+    function mostrar_value($value) {
+        if(isset($_POST[$value])) {
+            echo 'value="'.$_POST[$value].'"';
+        }
+    }
+    
+    function mostrar_error_campo($campo, $errores) {
+        if(isset($errores[$campo])) {
+            echo '<label class="label-errores">'.$errores[$campo].'</label>';
+        }
+    }
+    
+    function checked($name, $value) {
+        if(isset($_POST[$name])){
+            if ($_POST[$name] == $value) {
+                echo "checked";
+            }
+        }
+    }
+    
+    function selected($name, $value) {
+        if(isset($_POST[$name])){
+            if($_POST[$name] == $value) {
+                echo 'selected';
+            }
+        }
+    }
+    
+    function mostrar_text_area($name){
+        if(isset($_POST[$name])){
+            echo $_POST[$name];
+        }
+    }    
+    
+    // function comprobar_campo_vacio($campo, $nombre_campo){
+    //     if(empty($campo)){
+    //        return 'El campo '.$nombre_campo.' no debe estar vacio';
+    //     }
+    // }
+    
+    // function comprobar_errores_registro($dni, $nombre, $apellidos, $fecha_nacimiento, $telefono, $email, $contrasenia, $repetir_contrasenia, $errores) {
+    //     $error = [];
+    //     $error['dni'] = comprobar_campo_vacio($dni, 'DNI');
+    //     $error['nombre'] = comprobar_campo_vacio($nombre, 'Nombre');
+    //     $error['apellidos'] = comprobar_campo_vacio($apellidos, 'Apellidos');
+    //     $error['fecha_nacimiento'] = comprobar_campo_vacio($fecha_nacimiento, 'Fecha de nacimiento');
+    //     $error['telefono'] = comprobar_campo_vacio($telefono, 'Teléfono');
+    //     $error['email'] = comprobar_campo_vacio($email, 'Email');
+    //     $error['contrasenia'] = comprobar_campo_vacio($contrasenia, 'Contraseña');
+    //     $error['repetir_contrasenia'] = comprobar_campo_vacio($repetir_contrasenia, 'Repetir contraseña');
+
+    //     return $error;
+    // }
+    
+    function comprobar_errores_registro($dni, $nombre, $apellidos, $fecha_nacimiento, $telefono, $email, $contrasenia, $repetir_contrasenia, $errores) {
+        if(empty($dni)){
+            $errores['dni'] = 'El dni no debe estar vacio';
+        }
+        
+        if(empty($nombre)){
+            $errores['nombre'] = 'El nombre no debe estar vacio';
+        }
+
+        if(empty($apellidos)){
+            $errores['apellidos'] = 'Los apellidos no debe estar vacio';
+        }
+
+        if(empty($fecha_nacimiento)){
+            $errores['fecha_nacimiento'] = 'La fecha de nacimiento no debe estar vacio';
+        }
+
+        if(empty($telefono)){
+            $errores['telefono'] = 'El telefono no debe estar vacio';
+        }
+
+        if(empty($email)){
+            $errores['email'] = 'El email no debe estar vacio';
+        }
+
+        if(empty($contrasenia)){
+            $errores['contrasenia'] = 'La contraseña no debe estar vacio';
+        }
+
+        if(empty($repetir_contrasenia)){
+            $errores['repetir_contrasenia'] = 'El campo repetir contraseña no debe estar vacio';
+        }
+
+        return $errores;
+    }
+
+    function comprobar_errores_login(){
+
+    }
+    
+?>
+
