0f372a7b448caf461b7c97e17acf255c9d3e98bb
edwin <pilines2011@gmail.com>
2021-03-19 10:21:01 +0100

    cambio de flecha

--- denuncia.php
+++ denuncia.php
@@ -18,8 +18,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/global/global.css">
-    <link rel="stylesheet" href="css/global/monitor.css">
+    <link rel="stylesheet" href="./css/global/global.css">
+    <link rel="stylesheet" href="./css/global/monitor.css">
     <!-- <link rel="stylesheet" href="./css/registro/monitor.css"> -->
     <link rel="stylesheet" href="./css/login/monitor.css">
     <link rel="stylesheet" href="./css/denuncia/monitor.css">
