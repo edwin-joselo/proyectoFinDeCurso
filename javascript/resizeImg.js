cd5dbe1669b7f315090940b6cd5c256d3c94961d
mrpedrotomas <mrpedrotomas@gmail.com>
2021-04-02 12:09:37 +0200

    resizeImg final

--- javascript/resizeImg.js
+++ javascript/resizeImg.js
@@ -24,52 +24,80 @@ function ResizeImage() {
         var reader = new FileReader();
         // Set the image once loaded into file reader
 
-        reader.onload= resizeImg();
+        reader.onload= function(e) {
 
-        reader.readAsDataURL(file);
-
-        const truers = true;
-
-        reader.onload= resizeImg();
+            var img = document.createElement("img");
+            img.src = e.target.result;
         
-    }
-}
+            var canvas = document.createElement("canvas");
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+        
+            var MAX_WIDTH = 400;
+            var MAX_HEIGHT = 400;
+            var width = img.width;
+            var height = img.height;
+        
+            if (width > height) {
+                if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width;
+                    width = MAX_WIDTH;
+                }
+            } else {
+                if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height;
+                    height = MAX_HEIGHT;
+                }
+            }
+            canvas.width = width;
+            canvas.height = height;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0, width, height);
+        
+            dataurl = canvas.toDataURL(file.type);
+            // document.getElementById('output').src = dataurl;
+            document.getElementById('outputHidden').value = dataurl;
+        
+        };
 
-function resizeImg(e){
-
-    var img = document.createElement("img");
-    img.src = e.target.result;
-
-    var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-
-    var MAX_WIDTH = 400;
-    var MAX_HEIGHT = 400;
-    var width = img.width;
-    var height = img.height;
-
-    if (width > height) {
-        if (width > MAX_WIDTH) {
-            height *= MAX_WIDTH / width;
-            width = MAX_WIDTH;
-        }
-    } else {
-        if (height > MAX_HEIGHT) {
-            width *= MAX_HEIGHT / height;
-            height = MAX_HEIGHT;
-        }
-    }
-    canvas.width = width;
-    canvas.height = height;
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0, width, height);
-
-    dataurl = canvas.toDataURL(file.type);
-    // document.getElementById('output').src = dataurl;
-    document.getElementById('outputHidden').value = dataurl;
-    
-    if(truers){
         reader.readAsDataURL(file);
+        
+        reader.onload= function(e) {
+
+            var img = document.createElement("img");
+            img.src = e.target.result;
+        
+            var canvas = document.createElement("canvas");
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0);
+        
+            var MAX_WIDTH = 400;
+            var MAX_HEIGHT = 400;
+            var width = img.width;
+            var height = img.height;
+        
+            if (width > height) {
+                if (width > MAX_WIDTH) {
+                    height *= MAX_WIDTH / width;
+                    width = MAX_WIDTH;
+                }
+            } else {
+                if (height > MAX_HEIGHT) {
+                    width *= MAX_HEIGHT / height;
+                    height = MAX_HEIGHT;
+                }
+            }
+            canvas.width = width;
+            canvas.height = height;
+            var ctx = canvas.getContext("2d");
+            ctx.drawImage(img, 0, 0, width, height);
+        
+            dataurl = canvas.toDataURL(file.type);
+            // document.getElementById('output').src = dataurl;
+            document.getElementById('outputHidden').value = dataurl;
+
+            reader.readAsDataURL(file);
+        };
     }
 }
+
