30d52a8e4cf39d82ea32778bb0ac3a7a81aa00b5
mrpedrotomas <mrpedrotomas@gmail.com>
2021-04-06 13:09:13 +0200

    datatable movil

--- admin/datatable.php
+++ admin/datatable.php
@@ -6,13 +6,13 @@
     require_once '../php/funciones.php';
 
 
-    if(!isset($_SESSION['policia'])){
-        include_once './../php/redireccionar_index.php';
-    }
+    // if(!isset($_SESSION['policia'])){
+    //     include_once './../php/redireccionar_index.php';
+    // }
     
-    if(isset($_POST['cerrar_sesion'])){
-        unset($_SESSION['policia']);
-    }
+    // if(isset($_POST['cerrar_sesion'])){
+    //     unset($_SESSION['policia']);
+    // }
     
     $conexion = abrir_conexion_PDO();
 
@@ -30,41 +30,33 @@
     <!-- GLOBAL -->
     <link rel="stylesheet" href="../css/global/global.css">
     <link rel="stylesheet" href="../css/global/monitor.css">
-    <link rel="stylesheet" href="../css/global/movil.css">
     
-    <!-- MAQUETACIÓN USUARIO -->
+     <!-- MAQUETACIÓN USUARIO -->
     <link rel="stylesheet" href="../css/login/monitor.css">
-
-    <link rel="stylesheet" href="../css/login/movil.css">
 
     <!-- MAQUETACIÓN -->
     <link rel="stylesheet" href="./css/listar_denuncias/monitor.css">
-
-    <!-- <link rel="stylesheet" href="./css/listar_denuncias/monitor.css"> -->
 
     <!-- SWEETALERT2 -->
     <link rel="stylesheet" href="../css/sweetalert2/dark.css">
     <script src="../javascript/sweetalert2.js"></script>
 
-    <!-- Datatables -->
-    <link rel="stylesheet" href="../css/datatables/jquery.dataTables.css">
-
-    
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/rowreorder/1.2.7/css/rowReorder.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.7/css/responsive.dataTables.min.css">
 
+    <!-- Datatables -->
+    <!-- <link rel="stylesheet" href="../css/datatables/jquery.dataTables.css"> -->
 
 </head>
 <body>
     <?php
-    include_once './maquetacion/menu/cabecera.php';
-    include_once './maquetacion/listar_denuncias/principal.php';
+    include_once './maquetacion/datatable/principal.php';
     ?>
 
-    <!-- JQUERY -->
     <script src="../javascript/datatables/jquery.js"></script>
     <script src="../javascript/datatables/jquery.dataTables.js"></script>
+    
     <script src="https://cdn.datatables.net/rowreorder/1.2.7/js/dataTables.rowReorder.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.7/js/dataTables.responsive.min.js"></script>
     <script>
@@ -73,28 +65,17 @@
                 rowReorder: {
                     selector: 'td:nth-child(2)'
                 },
-                responsive: true,
-                language: {
-                    info: 'Mostrando _START_-_END_ de _TOTAL_ registros',
-                    lengthMenu: 'Mostrar _MENU_ registros por página',
-                    zeroRecords: 'No se han encontrado registros',
-                    infoEmpty: 'No hay registros disponibles',
-                    infoFiltered: '(filtrado de _MAX_ registros totales)',
-                    search: 'Criterio de búsqueda:',
-                    paginate: {
-                        previous: 'Anterior',
-                        next: 'Siguiente',
-                        first: 'Primera',
-                        last: 'Última'
-                    }
-                }
+                responsive: true
             } );
         } );
     </script>
+
+
+        <!-- JQUERY -->
     <!-- <script src="../javascript/datatables/tabla-traduccion.js"></script> -->
 
     <!-- JAVASCRIPT -->
-    <script src="../javascript/login.js"></script>
-    <script src="../javascript/cabecera_admin.js"></script>
+    <!-- <script src="../javascript/login.js"></script>
+    <script src="../javascript/cabecera_admin.js"></script> -->
 </body>
 </html>
