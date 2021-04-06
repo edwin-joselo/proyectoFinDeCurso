f4f8d97aa6dd6d55c0761dc49ce235df9cbe7bb2
edwin <pilines2011@gmail.com>
2021-04-06 14:13:09 +0200

    Nombre a las páginas

--- admin/delitos.php
+++ admin/delitos.php
@@ -25,7 +25,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Delitos</title>
 
     <!-- GLOBAL -->
     <link rel="stylesheet" href="../css/global/global.css">
