1ed10d6bd726c48bb37abe6188d954ea8b4599e5
edwin <pilines2011@gmail.com>
2021-04-08 17:05:06 +0200

    nose

--- maquetacion/index/pie.php
+++ maquetacion/index/pie.php
@@ -1,7 +1,4 @@
-<footer>
-    <!-- <img src="./imagenes/gobierno-de-españita.png"  alt=""> -->
-    <!-- <a href="https://es-es.facebook.com/PoliciaNacional/"><img src="./imagenes/icono-facebook.svg" alt=""></a> -->
-    
+<footer>    
     <img src="./imagenes/gobierno-de-españita.png" alt="" srcset="">
     <p>© Dirección General de la Policía 2021</p>
 </footer>
